293774e7c555c1e8be4667166889b9ad220b4cb6
vagrant <vagrant@localhost.localdomain>
2018-08-12 16:37:38 +0000

    Initial commit

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -5,5 +5,18 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
-
+function retrieve(){
+	console.log(window.path);
+        fetch(window.path).then(function(response){
+        return response.json();
+})
+.then(function(myJson) {
+        console.log(myJson);
+})
+.catch(function(error){
+	console.log(error);
+});
+}
 export default retrieve;
+
+
